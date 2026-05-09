10d38bfb1e9c54b9bbab9bbc0ada95137466968d
solivalle <127651078+solivalle@users.noreply.github.com>
2026-05-09 13:26:25 -0600

    initial commit

---
+++ infra/envs/dev/dev.tfvars
@@ -0,0 +1,4 @@
+environment   = "dev"
+project_name  = "pdds-2-quarter-project-group-6"
+bucket_name = "pdds-2-quarter-project-group-6-s3"
+region        = "us-west-2"
---
+++ infra/variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+}
